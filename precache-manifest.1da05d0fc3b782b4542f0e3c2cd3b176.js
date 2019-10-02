c42f33bdde34796a45a226511fc53f064cc1c11f
zaheer <zaheermalik284@gmail.com>
2019-10-02 11:26:20 +0500

    Updates

---
+++ precache-manifest.1da05d0fc3b782b4542f0e3c2cd3b176.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea52508707f37dcfe2e9c44d27ce1a56",
+    "url": "/github-pages/index.html"
+  },
+  {
+    "revision": "6cf6d7a2ec8824556523",
+    "url": "/github-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "335d8a23da263e37935b",
+    "url": "/github-pages/static/js/2.4835bc22.chunk.js"
+  },
+  {
+    "revision": "6cf6d7a2ec8824556523",
+    "url": "/github-pages/static/js/main.c95ec32d.chunk.js"
+  },
+  {
+    "revision": "03ff80cfa575fd8ab02e",
+    "url": "/github-pages/static/js/runtime-main.7fc9e64e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/github-pages/static/media/logo.25bf045c.svg"
+  }
+]);
