8b07e892862f1461d3ec450e79fe56942ce48739
zaheer <zaheermalik284@gmail.com>
2019-10-03 11:28:48 +0500

    tweeks

--- src/App.js
+++ src/App.js
@@ -6,17 +6,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={`${process.env.PUBLIC_URL}/images/logo.png`} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         My First Progressive Web APP
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          className="App-link" href="https://www.linkedin.com/in/zaheer-abbas-9a6aa8105/" target="_blank" rel="noopener noreferrer" >
+          Zaheer Alvi
         </a>
       </header>
     </div>
